7757d498e1af961ae27046dc0f5f5648babe4626
yama3ki <yama3ki.tada4@gmail.com>
2018-10-20 03:42:52 +0900

    1st commit

---
+++ src/BookTable.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="bookTable">
+        <template v-for="(book, idx) in books">
+            <droppable :key="idx">
+                <draggable>
+                    <img :src="book" />
+                </draggable>
+            </droppable>
+        </template>
+    </div>
+</template>
+<script>
+import Draggable from './Draggable.vue';
+import Droppable from './Droppable.vue';
+export default {
+    computed: {
+        books: function() { return this.$store.getters.books; }
+    },
+    components: {
+        Draggable, Droppable
+    }
+}
+</script>
+<style scoped>
+#bookTable {
+    display: grid;
+    grid-template-columns: repeat(3, 200px);
+    grid-auto-rows: minmax(200px, auto);
+    gap: 8px 8px;
+}
+#bookTable > div {
+    justify-self: center;
+    align-self: center;
+}
+</style>
